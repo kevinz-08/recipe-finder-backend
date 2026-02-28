4bfed42591ccf14ba14d4bd95a49695d0258a229
kevinz-08 <gutierrezamorochokevinsantiago@gmail.com>
2026-02-28 16:43:46 -0500

    fix: :bug: Bug: When the user refreshed the page, the session was closed. Solution: Added authenticate middleware to user route

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,9 +1,10 @@
 import { Router, Request, Response } from "express";
 import jsonresponse from "../lib/jsonresponse";
+import authenticate from "../auth/authenticate";
 
 const router = Router();
 
-router.get("/", (req: Request, res: Response): Response => {
+router.get("/", authenticate, (req: Request, res: Response): Response => {
   return res.status(200).json(jsonresponse(200, req.user));
 });
 
